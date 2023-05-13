ae7ef7faff673189b45d4a46e6bdcd9c8bd771ab
haroldCoder <haroldc2005@gmail.com>
2023-05-13 08:40:18 -0500

    origin cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
 app.use(cors({
-    origin: 'https://twitter-koderx-clone.vercel.app',
+    origin: '*',
 }));
 
 app.use("/apitwt", require("./routes/users.route"));
