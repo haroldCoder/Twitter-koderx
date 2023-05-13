7db2f6b0702a78b2d1d13dff33418e522bfd8e74
haroldCoder <haroldc2005@gmail.com>
2023-05-13 08:30:04 -0500

    cors fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,12 +9,11 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
 app.use(cors({
     origin: 'https://twitter-koderx-clone.vercel.app',
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-    credentials: true
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS']
 }));
 
-app.use("/apitwt", cors(),  require("./routes/users.route"));
-app.use("/apitwt", cors(),  require("./routes/tweets.route"))
+app.use("/apitwt", require("./routes/users.route"));
+app.use("/apitwt", require("./routes/tweets.route"))
 
 app.listen(PORT, (err, res)=>{
     if(err) throw err;
