2c83b708c704285287bb4747d2b13d1833f77793
haroldCoder <haroldc2005@gmail.com>
2023-05-13 08:23:10 -0500

    cors in routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,8 +9,8 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
 app.use(cors());
 
-app.use("/apitwt", require("./routes/users.route"));
-app.use("/apitwt", require("./routes/tweets.route"))
+app.use("/apitwt", cors(),  require("./routes/users.route"));
+app.use("/apitwt", cors(),  require("./routes/tweets.route"))
 
 app.listen(PORT, (err, res)=>{
     if(err) throw err;
